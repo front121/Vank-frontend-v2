0c1b4281f019ccf3802f6527d9f92677efaed592
Jheison rua <jheisonruapachon7@gmail.com>
2024-02-29 09:38:02 -0500

    added CI-CD 7

--- src/assets/Icon/IconsSidebar/IconSun.jsx
+++ src/assets/Icon/IconsSidebar/IconSun.jsx
@@ -6,7 +6,7 @@ export const IconSun = ({className}) => {
       width="25"
       height="24"
       viewBox="0 0 25 24"
-      fill="none"
+      
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
