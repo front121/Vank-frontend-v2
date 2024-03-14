3969147552e4772e34bfb02cf098428701e791ac
Daniel Aldana <frontend@thisisvank.com>
2024-03-14 01:27:17 -0500

    remove params

--- src/apps/Vank/Page/Transactions/Modal/Modal.tsx
+++ src/apps/Vank/Page/Transactions/Modal/Modal.tsx
@@ -6,14 +6,12 @@ import { useNavigate} from "react-router-dom";
 import { useTranslation } from "react-i18next";
 export const Modal = ({
   moreStyle,
-  title,
   monto,
   volver,
   data,
   transactionId,
 }: {
   moreStyle?: any;
-  title?: any;
   volver?: any;
   monto?: any,
   data?: any,
