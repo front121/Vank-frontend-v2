e77d1275b6ebf376b3b221850c3dab9c999d0638
estiven <stivencv7@gmail.com>
2024-03-15 11:02:30 -0500

    resolviendo conflictos

--- src/apps/Auth/SingUp/MultiStepFormView/Default.ts
+++
@@ -1,8 +0,0 @@
-export const Default = {
-  country: "Colombia",
-  date: "02-01-2002",
-  documentNumber: "1007492034",
-  firstName: "JOEL ALEJANDRO",
-  lastName: "PEREZ MONTANA",
-  gender: "M",
-};
